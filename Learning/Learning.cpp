ecd06dd414aab89415573fceaff1698824218b80
djvuelo <dj.vuelo@gmail.com>
2022-08-09 21:53:32 +0300

    Add project files.

---
+++ Learning/Learning.cpp
@@ -0,0 +1,11 @@
+// Learning.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://hackingcpp.com/index.html
+
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+}
