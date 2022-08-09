6f5e47a2e615c509fa59d7600861750bdb6650b6
djvuelo <dj.vuelo@gmail.com>
2022-08-09 21:55:00 +0300

    Test

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -2,10 +2,15 @@
 // https://hackingcpp.com/index.html
 
 #include <iostream>
-#include <vector>
 
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
+	
+	int a = 5;
+	a & 1 ? std::cout << "ne chet" : std::cout << "chet";
+
+
+
 }
