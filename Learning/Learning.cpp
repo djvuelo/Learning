3f3453a8e0b5f16c7f48b0ca59e0a0fd8f23f622
Vladimir Mikhailov <dj.vuelo@gmail.com>
2022-08-10 04:59:18 +0300

    Some functions

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -1,14 +1,56 @@
 // Learning.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // https://hackingcpp.com/index.html
 #include <iostream>
+#include <vector>
+#include <limits>
 
+
+void printVector (std::vector<int>);
+std::vector<int> garbageFirst ();
+void usingStdStringAndLiterals ();
+using indexes = std::vector<int>;
+using namespace std::string_literals;
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
 	
-	short a;
-	std::cin >> a;
-	a & 1 ? std::cout << "ne chet" : std::cout << "chet";
 
+	//printVector(garbageFirst());
+	// usingStdStringAndLiterals();
+}
+
+std::vector<int> garbageFirst()
+{
+	indexes a{};
+	short b;
+
+	std::cout << "Сколькими цифрами заполнить list? (ограничение " 
+	<< std::numeric_limits<short>::max() << " строк):";
+
+	std::cin >> b;
+
+	for(int i = 0; i < b; i++)
+	{
+		a.push_back(i);
+	}
+	return a;
+}
+
+void printVector(std::vector<int> indexes)
+{
+	for(int i: indexes)
+	{
+		std::cout << i << "\n";
+	}
+}
+
+void usingStdStringAndLiterals()
+{
+	auto test{"Hello world"s};
+	test[0] = 'h';
+	test.insert(5, "v4");
+	test += "kek";
+
+	std::cout << test;
 }
